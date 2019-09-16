a5b9b9fdad129502830ee47b4fb7a9b1ce275a8b
fushengcf <1726744044@qq.com>
2019-09-16 11:11:00 +0800

    touch program

---
+++ src/pages/Index.vue
@@ -0,0 +1,54 @@
+<template>
+
+  <div class="q-pa-md q-gutter-sm">
+    <q-btn label="Alert" color="primary" @click="alert" />
+    <q-btn label="Prompt" color="primary" @click="prompt" />
+  </div>
+
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+
+    }
+  },
+  methods: {
+    prompt () {
+      this.$q.dialog({
+        title: 'Prompt',
+        message: 'What is your name?',
+        prompt: {
+          model: '',
+          type: 'text' // optional
+        },
+        cancel: true,
+        persistent: true
+      }).onOk(data => {
+        console.log('>>>> OK, received', data)
+      }).onCancel(() => {
+        console.log('>>>> Cancel')
+      }).onDismiss(() => {
+        console.log('I am triggered on both OK and Cancel')
+      })
+    },
+    alert () {
+      this.$q.dialog({
+        title: 'Alert',
+        message: 'Some message'
+      }).onOk(() => {
+        console.log('OK')
+      }).onCancel(() => {
+        console.log('Cancel')
+      }).onDismiss(() => {
+        console.log('I am triggered on both OK and Cancel')
+      })
+    }
+  }
+}
+</script>
